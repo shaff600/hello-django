f53482de871993fe08db138dd1a251a3d33eb31c
shaff600 <shafyl@live.co.uk>
2021-11-29 16:52:21 +0000

    debug

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEVELOPMENT")
+# DEBUG = os.environ.get("DEVELOPMENT")
+DEBUG = True
 
 if os.environ.get("DEVELOPMENT"):
     ALLOWED_HOSTS = ['localhost']
