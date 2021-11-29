796e29ef3671da1b954ad14d82b953f2ec7f8e3d
shaff600 <shafyl@live.co.uk>
2021-11-29 15:34:29 +0000

    dev

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if DEVELOPMENT:
+if os.environ.get("DEVELOPMENT"): 
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
